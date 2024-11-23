403a655db15d63cbd764d16807169e8924caa8f2
“Laurent <laurentlebarbier70@gmail.com>
2024-11-23 19:14:48 +0100

    Initial commit for defisjs_2

---
+++ script.js
@@ -0,0 +1,10 @@
+function check() {
+    var nbr;
+    nbr = Number(document.getElementById("age").value);
+    if (nbr < 18) {
+        alert("Vous n'êtes pas un adulte");
+    }
+    else {
+        alert("Vous êtes un adulte");
+    }
+}
